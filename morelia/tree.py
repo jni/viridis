6315ed30820c6d90dd0eb5637674bec22722fa89
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2012-06-19 19:32:55 -0400

    Add LCA algorithm and tree split function

--- morelia/tree.py
+++ morelia/tree.py
@@ -13,6 +13,18 @@ from scipy.misc import comb as nchoosek
 
 # local modules
 
+def lowest_common_ancestor(t, u, v):
+    au = t.ancestors(u)
+    av = t.ancestors(v)
+    common = set(au) & set(av)
+    short = au if len(au) < len(av) else av
+    for s in short:
+        if s in common:
+            return s
+    return None
+
+lca = lowest_common_ancestor
+
 class Ultrametric(nx.DiGraph):
     """An ultrametric tree data structure"""
     def __init__(self, init_nodes=[]):
@@ -21,13 +33,23 @@ class Ultrametric(nx.DiGraph):
         self.add_nodes_from(init_nodes, w=self.maxw, num_leaves=1)
         self.id_counter = it.count(max([0] + init_nodes) + 1)
 
-    def merge(self, n1, n2, w=0.0):
+    def merge(self, u, v, w=0.0):
         node_id = self.id_counter.next()
         self.maxw = max(w, self.maxw)
-        num_leaves = self.num_leaves(n1) + self.num_leaves(n2)
+        num_leaves = self.num_leaves(u) + self.num_leaves(v)
         self.add_node(node_id, w=self.maxw, num_leaves=num_leaves)
-        self.add_edges_from([(node_id, n1), (node_id, n2)])
+        self.add_edges_from([(node_id, u), (node_id, v)])
         return node_id
+
+    def split(self, u, v):
+        split_node = lowest_common_ancestor(self, u, v)
+        if split_node is None:
+            return
+        a = self.ancestors(split_node)
+        num_leaves = self.node[split_node]['num_leaves']
+        self.remove_node(split_node)
+        for n in a:
+            self.node[n]['num_leaves'] -= num_leaves
 
     def get_map(self, t=np.inf):
         """Compute a map from leaf nodes to roots at a certain height."""
@@ -51,6 +73,23 @@ class Ultrametric(nx.DiGraph):
 
     def num_leaves(self, n):
         return self.node[n]['num_leaves']
+
+    def parent(self, n):
+        p = self.predecessors(n)
+        if len(p) == 1:
+            return p[0]
+        return None
+
+    def ancestors(self, n):
+        a = []
+        node = n
+        while self.parent(node) is not None:
+            node = self.parent(node)
+            a.append(node)
+        return a
+
+    def children(self, n):
+        return self.successors(n)
 
 def num_leaves(g, n):
     return g.node[n]['num_leaves']
