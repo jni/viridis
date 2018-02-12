d48e164374af0054def74d15a0150dc32d6b3661
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2018-02-12 14:55:54 +1100

    Bump version to 0.4.2-dev

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ MAINTAINER_EMAIL    = 'juan.n@unimelb.edu.au'
 URL                 = 'https://github.com/jni/viridis'
 LICENSE             = 'BSD 3-clause'
 DOWNLOAD_URL        = 'https://github.com/jni/viridis'
-VERSION             = '0.4.1'
+VERSION             = '0.4.2-dev'
 PYTHON_VERSION      = (3, 5)
 INST_DEPENDENCIES   = {}
 
